72cca21acc7366cec4e0e15452ab3bbf2ad1aff8
Baturay Acarturk <baturayacarturk@outlook.com>
2024-05-05 04:32:22 +0300

    read 32 bits at once

--- gateway.py
+++ gateway.py
@@ -1,23 +1,24 @@
 import serial
 import time
-# Open COM port
-ser = serial.Serial('COM5', 9600)  # Adjust baud rate as per your device
 
-while 1:
+# Open COM ports for reading and writing
+receive = serial.Serial('COM5', 9600)  # Adjust baud rate as per your device
+send = serial.Serial('COM6', 9600)
+
+binary_sequence = '10000011' + '01100001' + '01100111' + '10000101'
+bytes_to_send = bytes([int(binary_sequence[i:i+8], 2) for i in range(0, len(binary_sequence), 8)])
+send.write(bytes_to_send)
+
+while True:
     try:
-        received_data = ser.read(4)
+        received_data = receive.read(4)
         for byte in received_data:
-
             byte_str = format(byte, '08b')  # Convert to 8-bit binary string
-
-            start_bit = byte_str[0]
             stop_bit = byte_str[-1]
-            
-            data_bits = byte_str[1:-1]
-
+            data_bits = byte_str[:-1]
             char = chr(int(data_bits, 2))
-            output_string = f"Start Bit: {start_bit}, Data Bits: {data_bits}, Stop Bit: {stop_bit}, Character: {char}\n"
-
-            ser.write(output_string.encode('ascii'))
+            output_string = f"Data Bits: {data_bits}, Stop Bit: {stop_bit}, Character: {char}\n"
+            print(output_string.encode('ascii'))
+            # TODO variable'a ekle. Komutları match et endpoint ile... istek için 
     except Exception as e:
         print(e)
